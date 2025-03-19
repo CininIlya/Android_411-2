6b907985cd3e5089fe6d848f42169d75714da112
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-03-19 19:24:39 +0800

    buttoms

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,27 +1,37 @@
 package com.example.first;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textSample;
+    private TextView textSample2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-       setContentView(R.layout.first_layout);
-//        setContentView(R.layout.second_layaout);
-//        setContentView(R.layout.liner_layout);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.buttons);
+
+
+        textSample = findViewById(R.id.textSample);
+        textSample2 = findViewById(R.id.textSample2);
+
+
+
+    }
+
+    public void sayHello(View view){
+        textSample.setText("Привет");
+    }
+    public void sayHello2(View view){
+        textSample2.setText("Hello");
+
+
     }
 }
