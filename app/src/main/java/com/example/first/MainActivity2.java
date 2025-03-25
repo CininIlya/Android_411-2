1b8eb65766e9892a4130019e6f9cea08aa92ce99
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-03-25 20:29:34 +0800

    calculator

--- app/src/main/java/com/example/first/MainActivity2.java
+++ app/src/main/java/com/example/first/MainActivity2.java
@@ -1,24 +1,72 @@
 package com.example.first;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView result;
+    private EditText num1,num2;
+    private Button button;
+    private Button buttonMinus;
+    private Button buttonMultiple;
+    private Button buttonDivision;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_calc);
+
+        result = findViewById(R.id.result);
+        num1 = findViewById(R.id.editNumber1);
+        num2 = findViewById(R.id.editNumber2);
+        button= findViewById(R.id.calc_Sum);
+        buttonMinus =findViewById(R.id.calc_Minus);
+        buttonMultiple =findViewById(R.id.calc_Mult);
+        buttonDivision =findViewById(R.id.calc_division);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Integer number1 = Integer.parseInt(num1.getText().toString());
+               Integer number2 = Integer.parseInt(num2.getText().toString());
+               result.setText(String.valueOf(number1 + number2));
+
+
+            }
+        });
+       buttonMinus.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Integer number1 = Integer.parseInt(num1.getText().toString());
+               Integer number2 = Integer.parseInt(num2.getText().toString());
+               result.setText(String.valueOf(number1 - number2));
+           }
+       });
+        buttonMultiple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer number1 = Integer.parseInt(num1.getText().toString());
+                Integer number2 = Integer.parseInt(num2.getText().toString());
+                result.setText(String.valueOf(number1 * number2));
+            }
+        });
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer number1 = Integer.parseInt(num1.getText().toString());
+                Integer number2 = Integer.parseInt(num2.getText().toString());
+                result.setText(String.valueOf(number1 / number2));
+
+            }
         });
     }
 }
