8c5f29a5a9f949d88a1a7cf40f20ff8424fa769c
ilacin83655@gmail.com <ilacin83655@gmail.com>
2025-07-22 21:56:13 +0800

    sd card

---
+++ FileManagerProjectAplication/app/src/main/java/com/example/filemanagerprojectaplication/FileAdapter.java
@@ -0,0 +1,4 @@
+package com.example.filemanagerprojectaplication;
+
+public class FileAdapter {
+}
---
+++ FileManagerProjectAplication/app/src/main/java/com/example/filemanagerprojectaplication/OneFileSelectedListener.java
@@ -0,0 +1,4 @@
+package com.example.filemanagerprojectaplication;
+
+public interface OneFileSelectedListener {
+}
